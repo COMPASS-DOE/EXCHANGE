9a2f031931b4654fbb40c2bfe3e4959f7ceeef7e
Stephanie Pennington <spenn1@terpmail.umd.edu>
2022-02-01 16:25:54 -0500

    sig figs updates and reports Eh instead of orp_m

--- Processing_Scripts/water_quality.R
+++ Processing_Scripts/water_quality.R
@@ -36,30 +36,39 @@ titrator_path = "https://docs.google.com/spreadsheets/d/1lP4ft29oknaR5Xthv3Qo29E
 ## correct for this error, alkalinity measurements in triplicate for the
 ## wrong acid and new, correct acid were run, and a correction factor of 4.02 
 ## was determined. This is incorporated into the alk_mgl_
+##
+## Salinity is calculated from spcond using the GSW toolbox
+## Eh is calculated from orp_mv using Eh = orp_mv + 207, per sensor manual
 titrator_data <- read_sheet(titrator_path, col_types = 'Tcddddddc') %>% 
   mutate(sal_psu = gsw_SP_from_C(C = spcond_mscm, t = 25, p = 0), 
+         eh = orp_mv + 207, #Eh = mV + 207 (per sensor manual)
          alk_mgl_caco3 = ifelse(hcl_molarity == 0.02, (ml_hcl_added * hcl_molarity * 50000) / 50, 
                              ifelse(hcl_molarity == 0.1, (ml_hcl_added * hcl_molarity * 50000) / 50 / 4.02, NA))) %>% 
-  dplyr::select(kit_id, sal_psu, ph, orp_mv, alk_mgl_caco3)
+  dplyr::select(kit_id, sal_psu, ph, eh, alk_mgl_caco3)
 
 # 3. QC data -------------------------------------------------------------------
 
 clean_data <- function(data) {
   data %>% 
+    ## First, round each parameter to proper significant figures
+    mutate(sal_psu = round(sal_psu, 2), 
+           ph = round(ph, 2), 
+           eh = round(eh, 0), 
+           alk_mgl_caco3 = round(alk_mgl_caco3, 0)) %>% 
+    ## Second, add flags for 
     mutate(f_sal_psu = sal_psu < 0 | sal_psu > 50, 
            f_ph = ph < 0 | ph > 14, 
-           f_orp_mv = orp_mv < 0 | orp_mv > 500,
-           f_alk_mgl_caco3 = alk_mgl_caco3 > 0)
+           f_eh = eh < 0,
+           f_alk_mgl_caco3 = alk_mgl_caco3 < 0)
 }
 
-titrator <- clean_data(titrator_data)
+titrator <- clean_data(titrator_data) %>% 
+  drop_na()
 
 
 # 4. Write L0B data to Google Drive ---------------------------------------------
-write_csv(titrator, "data/220121_water_quality_L0B.csv")
+write_csv(titrator, "data/EC1_Water_WaterQuality_L0B.csv")
 
-
-# 5. Clean out manually inspected 
 
 
 
