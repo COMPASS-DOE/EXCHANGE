7f5e35f77f253be3e70e9fda7b616dea0bad5aea
Stephanie Pennington <spenn1@terpmail.umd.edu>
2022-03-20 13:14:56 -0400

    Search criteria

--- Explorer/ui.R
+++ Explorer/ui.R
@@ -14,7 +14,27 @@ shinyUI(
 
                         leafletOutput("map", height = "calc(100vh - 80px)", width = "100%")
                         ),
-               tabPanel("Data"),
+               tabPanel("Data",
+                        # Placeholder search criteria
+                        fluidRow(
+                        
+                          column(3,
+                                 selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
+                          ),
+                          column(3,
+                                 conditionalPanel("input.states",
+                                                  selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+                                 )
+                          ),
+                          column(3,
+                                 conditionalPanel("input.states",
+                                                  selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+                                 )
+                          )
+                          ),
+                        hr(),
+                        DT::dataTableOutput("ziptable")
+                        ),
                tabPanel("Get Involved")
 
     )
