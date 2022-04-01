21d60ae29ee3824c752bc8bb194fd925bda3aaf1
Stephanie Pennington <spenn1@terpmail.umd.edu>
2022-04-01 15:07:28 -0400

    Add hover panel on map

--- Explorer/ui.R
+++ Explorer/ui.R
@@ -8,27 +8,56 @@ library(shinythemes)
 shinyUI(
 
     navbarPage("EXCHANGE Explorer",
+               
                theme = "lumen",
+               tags$head(
+                 # Include our custom CSS
+                 includeCSS("style.css")
+               ),
                tabPanel("Home"),
                tabPanel("Map",
 
-                        leafletOutput("map", height = "calc(100vh - 80px)", width = "100%")
+                        leafletOutput("map", height = "calc(100vh - 80px)", width = "100%"),
+                        
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 90, left = "auto", right = 30, bottom = "auto",
+                                      width = 330, height = "auto",
+                                      
+                                      h2("Campaign Explorer")#,
+                                      
+                                      # selectInput("color", "Color"),
+                                      # selectInput("size", "Size"),
+                                      # conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                      #                  # Only prompt for threshold when coloring or sizing by superzip
+                                      #                  numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                                      # )
+                        )
+                        
                         ),
                tabPanel("Data",
                         # Placeholder search criteria
                         fluidRow(
                         
                           column(3,
-                                 selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
+                                 selectInput("states", 
+                                             "States", 
+                                             c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), 
+                                             multiple=TRUE)
                           ),
                           column(3,
                                  conditionalPanel("input.states",
-                                                  selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+                                                  selectInput("cities", 
+                                                              "Cities", 
+                                                              c("All cities"=""), 
+                                                              multiple=TRUE)
                                  )
                           ),
                           column(3,
                                  conditionalPanel("input.states",
-                                                  selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+                                                  selectInput("zipcodes", 
+                                                              "Zipcodes", 
+                                                              c("All zipcodes"=""), 
+                                                              multiple=TRUE)
                                  )
                           )
                           ),
