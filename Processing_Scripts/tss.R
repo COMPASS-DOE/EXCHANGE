b5ca856b6ca51712f691da41c0c28b0d6055239e
Stephanie Pennington <spenn1@terpmail.umd.edu>
2022-05-19 16:29:40 -0400

    Add processing and qc

--- Processing_Scripts/tss.R
+++ Processing_Scripts/tss.R
@@ -24,18 +24,20 @@ pacman::p_load(cowsay,
                readr,
                tidyr,
                googlesheets4, # read_sheet 
-               lubridate) # drive_upload
+               lubridate,
+               gsw) # drive_upload
 
 ## Welcome
 say("Welcome to EXCHANGE!", by = "random")
 
 ## URL for data
-data_path = "https://docs.google.com/spreadsheets/d/13MBZYTh3K8bsog27UZOx2mXUGflXMukapq7Zfsl1ooM/edit#gid=768038889" 
+data_path <- "https://docs.google.com/spreadsheets/d/13MBZYTh3K8bsog27UZOx2mXUGflXMukapq7Zfsl1ooM/edit#gid=768038889" 
+titrator_path <- "https://docs.google.com/spreadsheets/d/1lP4ft29oknaR5Xthv3Qo29EZeco6R3WkRkH-TT_x1vc/edit#gid=1069041223"
 gsheet_tab <- "Sheet1"
 
 ## Define constants
-a_min = int
-a_max = int
+f4_min = 4
+f4_max = 10000
 
 ## Define analyte
 var <- "TSS"
@@ -44,8 +46,12 @@ var <- "TSS"
 # 2. Import data ---------------------------------------------------------------
 cat("Importing", var, "data...")
 
+## read in titrator data
+titrator <- read_sheet(ss = titrator_path, range = "Data")
+
 ## read in raw data
-data_raw <- read_sheet(ss = data_path, range = gsheet_tab, skip = 5)
+data_raw <- read_sheet(ss = data_path, range = gsheet_tab, skip = 5,
+                       col_types = "DDccddddddccDDDc", na = c("--", "?", ""))
 
 #
 # 3. Process data --------------------------------------------------------------
@@ -53,23 +59,51 @@ cat("Processing", var, "data...")
 
 data_raw %>% 
   drop_na(`Kit #`) %>% # remove blank rows used for visual separation
-  rename(Date_Received = `Date received`, Date_Filtered = `Date filtered`,
-         Kit_ID = `Kit #`) %>% 
-  group_by(Kit_ID) %>% 
+  rename(date_received = `Date received`, date_filtered = `Date filtered`,
+         kit_id = `Kit #`) %>% 
+  mutate(filter_mass_g = `Top filter + foil dry weight (g)` - `Top filter + foil tare weight (g)`,
+         bottom_filter_g = `Bottom filter + foil dry weight (g)` - `Bottom filter + foil tare weight (g)`) %>% 
+  group_by(kit_id, IGSN) %>% 
+  mutate(sample_weight_g = first(`Sample wt pre filtration (g)`) - last(`Sample wt post filtration (g)`)) %>% 
+  summarise(total_filter_mass_g = sum(filter_mass_g), 
+            total_bottom_filter_g = sum(bottom_filter_g),
+            sample_weight_g = first(sample_weight_g),
+            n = n()) -> processed_interim
 
+processed_interim %>% 
+  filter(grepl("Blank", kit_id)) %>% 
+  ungroup() %>% 
+  summarise(blank_avg_g = mean(total_filter_mass_g)) -> blank_avg_g
 
-
+processed_interim %>% 
+  filter(!grepl("Blank", kit_id)) %>% 
+  bind_cols(blank_avg_g) %>% 
+  mutate(total_filter_mass_g = ifelse(total_bottom_filter_g > 0, 
+                                      total_filter_mass_g - total_bottom_filter_g,
+                                      total_filter_mass_g)) -> x 
+  left_join(titrator, by = "kit_id") %>% 
+  mutate(transect_position = "Water",
+         SA = gsw_SA_from_SP(spcond_mscm, 0, longitude = -123.046, latitude = 48.078),
+         CT= gsw_CT_from_t(SA, temperature_c, 0),
+         density_water = gsw_rho(SA, CT, 0),
+         volume_filtered_ml = sample_weight_g * density_water,
+         tss_mg_perl = ((total_filter_mass_g / volume_filtered_ml) * 1000 * 1000) - blank_avg_g) -> data_processed
+ 
 #
 # 4. Apply QC flags ------------------------------------------------------------
 cat("Applying flags to", var, "data...")
 
 data_qc <- function(data) {
   data %>% 
-    mutate(a = round(a, n_sig_figs)) %>% 
-    mutate(f_a = a < a_min | a > a_max) 
+    mutate(flag_1 = ifelse(n > 1, T, F),
+           flag_2 = ifelse(total_filter_mass_g < 0, T, F),
+           flag_3 = ifelse(total_bottom_filter_g > 0, T, F),
+           flag_4 = ifelse(tss_mg_perl < f4_min | tss_mg_perl > f4_max, T, F)) 
 }
 
-data_clean <- data_qc(data_processed)
+data_qc(data_processed) %>% 
+  select(kit_id, transect_position, total_filter_mass_g, tss_mg_perl, volume_filtered_ml,
+         flag_1, flag_2, flag_3, flag_4) -> data_clean 
 
 #
 # 5. Write cleaned data to drive -----------------------------------------------
