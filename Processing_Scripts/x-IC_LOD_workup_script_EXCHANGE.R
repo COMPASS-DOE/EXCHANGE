26bf661b28da8ca76cb03e640bf9898e5fb2f356
amyerspigg <allison.myers-pigg@pnnl.gov>
2022-10-04 17:33:05 -0700

    Update x-IC_LOD_workup_script_EXCHANGE.R

--- Processing_Scripts/x-IC_LOD_workup_script_EXCHANGE.R
+++ Processing_Scripts/x-IC_LOD_workup_script_EXCHANGE.R
@@ -24,64 +24,180 @@
 #1. Establish Constants & Load Packages :
 z <- 3 # z = confidence interval (3 = 99%)
 
-library(tidyverse)
+require(pacman)
+pacman::p_load(cowsay,
+               tidyverse,
+               googlesheets4, # read_sheet 
+               googledrive # drive_upload
+)
 
 #### 2.  Import blank concentrations ####
 
 #2a. confirm working directory is ../GitHub/EXCHANGE#
 getwd()
 
-#2b. Create Function to read in blanks 
-read_blanks <- function(data){
-  # First, scrape date from filename
-  date <- str_extract(data, "[0-9]{8}")
-  # Second, read in data
-  read_delim(file = data, skip = 10, delim = "\t") %>% 
-    rename(sample_name = `Sample Name`, 
-           npoc_raw = `Result(NPOC)`, 
-           tdn_raw = `Result(TN)`,
-           run_datetime = `Date / Time`) %>% 
-    select(sample_name, npoc_raw, tdn_raw,run_datetime) %>% 
-    mutate(date = date)
+#2b. Import data 
+#Need:
+## URL for data
+# data_path = "xxxx" 
+
+## Define analyte
+var <- "ions"
+
+cat("Importing", var, "data...")
+
+# `import_data`: this function will import all xls files in the target directpry and combine them
+# input parameters are (a) FILEPATH, the target directory with the raw data files
+
+import_data = function(directory){
+  
+  ## a. Create a list of files to download
+  files <- 
+    drive_ls(directory) %>% 
+    filter(grepl("_Data_Raw_", name))
+  
+  ## b. Download files to local (don't worry, we'll delete em in a sec)
+  lapply(files$id, drive_download, overwrite = TRUE)
+  
+  
+  ## c. pull a list of file names
+  ## then read all files and combine
+  
+  filePaths <- files$id
+  dat <- 
+    do.call(rbind, lapply(filePaths, function(path){
+      # then add a new column `source` to denote the file name
+      df <- readxl::read_excel(path, skip = 2)
+      #  df <- read.delim(path, skip = 2)
+      df[["source"]] <- rep(path, nrow(df))
+      df}))
+  
+  ## d. delete the temporary files
+  file.remove(c(files$id))  
+  
+  ## e. output
+  dat
 }
-blanks_raw <- files %>% 
-  map_df(read_data) %>% 
-  filter(grepl("^Blank", sample_name)) %>% # filter to TMP samples only
-  bind_rows() 
 
+# Now, Import all the ions data:
+directory = "https://drive.google.com/drive/u/1/folders/1tp6X9bJYFjsc9fda5R8aGBhiNMUfpJsN"
 
-Sreag.avg <- aggregate(Area ~ Analyte + Date_Run, data = Blankfile, mean)
-σreag.sd <- aggregate(Area ~ Analyte + Date_Run, data = Blankfile, sd)
+raw_data = import_data(directory)
+
+#2d. Process BLANKS
+process_blanks = function(raw_data, IONS){
+  
+  # The input data are in shitty, non-tidy format, with multi-line headers and multiple chunks of data per dataframe.  
+  # This function assigns the ions and turns it into tidy format, then cleans/processes the dataframe
+  
+  # a. assign ions ----
+  
+  # identify the rows that contain ions names
+  label_rows = which(grepl(paste(IONS, collapse = "|"), raw_data$Time))
+  
+  # make this a dataframe/tibble
+  label_rows_df = 
+    label_rows %>% 
+    as_tibble() %>%
+    rename(Row_number = value) %>% 
+    mutate(label = TRUE, 
+           Row_number = as.character(Row_number))
+  
+  # now join this to the dataframe
+  data_new = 
+    raw_data %>% 
+    rownames_to_column("Row_number") %>% 
+    left_join(label_rows_df) %>% 
+    mutate(Ion = case_when(label ~ Area)) %>% 
+    # ^ this pulls the Ion name only for certain rows
+    # use fill() to down-fill the values
+    # it will down-fill until it hits the next non-empty cell
+    # therefore, make sure to include ALL ion names in the IONS parameter
+    fill(Ion) %>% 
+    dplyr::select(-Row_number, -label)
+  
+  # the dataframe now has all the ions assigned to each row
+  # but it is still horribly untidy
+  
+  # b. clean the dataframe -----
+  
+  # preliminary processing to make it tidy
+  blanks_new_processed = 
+    data_new %>% 
+    filter(!is.na(`No.`), 
+           grepl("Blank", Name)) %>% 
+    filter(!grepl("CondBlank", Name)) %>% #Remove conditioning blanks 
+    filter(!Name %in% c("Blank1", "Blank2", "Blank3", "Blank4")) %>% #filtering out carry over blanks 
+    mutate_at(vars(-Name, -Ion, -source), as.numeric) %>% 
+    # pull the date run from the long `source` column
+    mutate(date_run = str_extract(source, "[0-9]{8}"),
+           date_run = lubridate::as_date(date_run)) %>%
+    dplyr::select(Name, Area, Ion, date_run) %>% 
+    mutate(Ion = str_remove_all(Ion, "_UV")) %>% 
+    force()
+  
+  blanks_new_processed
+  
+}
+
+#assign ions of interest: 
+all_ions = c("Lithium", "Sodium", "Ammonium", "Potassium", "Magnesium", "Calcium", "Nitrite", "Nitrate",
+             "Chloride", "Bromide", "Sulfate", "Phosphate", "Fluoride")
+
+#Now run function: 
+blanks_ions_processed = process_blanks(raw_data, IONS = all_ions)
+
+#### 3.  Calculate averages and SDs of blank concentrations per run ####
+Sreag.avg <- aggregate(Area ~ Ion + date_run, data = blanks_ions_processed, mean)
+
+σreag.sd <- aggregate(Area ~ Ion + date_run, data = blanks_ions_processed, sd)  
 
 colnames(Sreag.avg)[colnames(Sreag.avg) == 'Area'] <- 'Sreag'
 colnames(σreag.sd)[colnames(σreag.sd) == 'Area'] <- 'σreag'
 
-SADL_cal <- dplyr::left_join(Sreag.avg,σreag.sd, by=c("Analyte","Date_Run"))
+SADL_cal <- dplyr::left_join(Sreag.avg,σreag.sd, by=c("Ion","date_run"))
+
+SADL_cal[is.na(SADL_cal)] = 0
 
 SADL_cal <- SADL_cal %>% dplyr::mutate(SADL = Sreag + (z*σreag))
 
-#### Bring in slope data #####
+#### 4.	Import slope of the standard curve for each ion – slope is the signal response   #####
 
-###need to assign ions - function modified for slopes from EXCHANGE (https://github.com/COMPASS-DOE/EXCHANGE/blob/kaizad/Processing_Scripts/ions.R)###
-assign_ions = function(FILEPATH, PATTERN, IONS){
+### Function to import slopes data:
+
+import_slopes = function(directory){
   
-  # a. read and combine files ----
+  ## a. Create a list of files to download
+  files <- 
+    drive_ls(directory) %>% 
+    filter(grepl("_Slope_", name))
   
-  # pull a list of file names in the target folder with the target pattern
-  # then read all files and combine
-  filePaths <- list.files(path = FILEPATH, pattern = PATTERN, full.names = TRUE)
-   #filePaths <- list.files(path= "/Users/myer056/OneDrive - PNNL/Data Generation and Files/Raw_Instrument_Data/IC-6000 MCRL/Data for LOD calculations/Slopes", pattern = c("RC2"), full.names = TRUE)
-   #IONS = all_ions
+  ## b. Download files to local (don't worry, we'll delete em in a sec)
+  lapply(files$id, drive_download, overwrite = TRUE)
   
-  #to troubleshoot: dplyr::bind_rows binds all rows, you can see which ones are not matching
+  
+  ## c. pull a list of file names
+  ## then read all files and combine
+  
+  filePaths <- files$id
   dat <- do.call(rbind, lapply(filePaths, function(path) {
     # then add a new column `source` to denote the file name
     df <- readxl::read_xls(path, skip = 7)
     df[["source"]] <- rep(path, nrow(df))
     df}))
   
-  # b. start processing the ions data ----
+  ## d. delete the temporary files
+  file.remove(c(files$id))  
   
+  ## e. output
+  dat
+}
+
+#import slopes:
+raw_slopes = import_data(slope.directory)
+slope.directory = "https://drive.google.com/drive/u/1/folders/1pf5oxzg15uB0twTl76sEGD0Ayan8ckBy"
+
+assign_slopes = function(raw_slopes, IONS){
   # identify the rows that contain ions names
   label_rows = which(grepl(paste(IONS, collapse = "|"), dat$`Peak Name`))
   
@@ -94,124 +210,36 @@ assign_ions = function(FILEPATH, PATTERN, IONS){
           Row_number = as.character(Row_number))
   
   # now join this to the dataframe
-  data_new <-  dat %>% 
+  slope_new <-  dat %>% 
     tibble::rownames_to_column("Row_number") %>% 
    right_join(label_rows_df) %>% 
-   # mutate(Ion = case_when(label ~ Slope)) %>% 
-    # ^ this pulls the Ion name only for certain rows
-    # use fill() to down-fill the values
-    # it will down-fill until it hits the next non-empty cell
-    # therefore, make sure to include ALL ion names in the IONS parameter
-   # tidyr::fill(Ion) %>% 
    dplyr::select(-Row_number, -label)
   
-  # create header by collapsing the header + first row
- # new_header = data_new %>% 
- #   colnames() %>% 
- #   paste0(data_new[1,]) %>% 
- #   str_remove_all("NA")
-  # the "source" column has "source" with a lot more crap
-  # use grepl to replace that full value with just "source"
- # new_header = replace(new_header, grep("source", new_header), "source")
-  
-  # set column names for the dataframe
-#  names(data_new) <- new_header  
-  
   # preliminary processing to make it tidy
-  data_new_processed = 
-    data_new %>% 
-    #filter(!is.na(`No.`)) %>% 
+  slope_new_processed = 
+    slope_new %>% 
     mutate_at(vars(-'Peak Name', -source, -Cal.Type, -Points, -Offset, -Slope, -Curve, -Coeff.Det., -Eval.Type), as.numeric) %>% 
     mutate(Date_Run.1 = str_extract(source, "[0-9]{8}"),
            Date_Run = lubridate::as_date(Date_Run.1)) %>% 
     dplyr::select('Peak Name', Slope, Date_Run) %>% 
     force()
   
-  data_new_processed
+  slope_new_processed
   
 }
 
-#output from slope file needs to be Analyte, Slope, Date
+#output from slope file needs to be Ion, Slope, Date
 
+m.run = assign_slopes(raw_slopes,IONS = all_ions)
 
-all_ions = c("Lithium", "Sodium", "Ammonium", "Potassium", "Magnesium", "Calcium", "Nitrite", "Nitrate",
-             "Chloride", "Bromide", "Sulfate", "Phosphate", "Fluoride")
-
-### NEED TO CHANGE FOR CAMPAIGN SPECIFIC CALCULATIONS! IF YOU WANT ALL THEN CHANGE PATTERN TO .xls
-
-m.run = assign_ions(FILEPATH = "/Users/myer056/OneDrive - PNNL/Data Generation and Files/Raw_Instrument_Data/IC-6000 MCRL/Data for LOD calculations/Slopes", 
-                                 PATTERN = "COMPASS",
-                                 IONS = all_ions)
-
-colnames(m.run)[colnames(m.run) == 'Peak Name'] <- 'Analyte'
+colnames(m.run)[colnames(m.run) == 'Peak Name'] <- 'Ion'
 
 
 #LOD.run = SADL_cal / m.run 
 
-data_m_sadl <- dplyr::left_join(SADL_cal,m.run, by=c("Analyte","Date_Run"))
+data_m_sadl <- dplyr::left_join(SADL_cal,m.run, by=c("Ion","Date_Run"))
 data_m_sadl <- data_m_sadl %>% filter(!is.na(Slope)) %>% 
   mutate(LOD.run = SADL / as.numeric(Slope))
 
-save(data_m_sadl, file="Prelim_all_LODs_byrun_COMPASS.rda")
+save(data_m_sadl, file="LODs_byrun_COMPASS.rda")
 
-#Maintenance Periods# 
-
-maintenance_dates <- readxl::read_excel(paste0(setuser,"/Documents/GitHub/EXCHANGE/Data/LODs/Maintenance_Dates_IC.xlsx"))
-
-dates = 
-  maintenance_dates %>%
-  distinct(Date) %>% # remove duplicates
-  add_row(Date = as.Date("2020-10-01")) %>% # add a "first date for the runs"
-  #add_row(Date=as.Date("2022-02-20")) %>%
-  add_row(Date = as.Date(Sys.Date())) %>% # set current date as the "final date for runs"
-  arrange(Date) %>% 
-  mutate(Beginning = Date,
-         Ending = lead(Beginning)) %>% # create columns denoting date range
-  drop_na() %>% 
-  rownames_to_column("group") # assign a group to each date range, easiest to just use row numbers
-
-data_bins = 
-  data_m_sadl %>% 
-  dplyr::select(Analyte, Date_Run, LOD.run)
-
-lod_dates = 
-  merge(data_bins, dates) %>% # this will merge all rows in file1 with all rows in file2
-  subset(Date_Run >= Beginning & Date_Run <= Ending)  # keep only rows where dates fall within the range
-
-lod_final.1 = aggregate(LOD.run ~ Analyte + Beginning + Ending, data=lod_dates, mean)
-
-lod_final = lod_final.1 %>%  
-  dplyr::rename(LOD_ppm = LOD.run) 
-
-View(lod_final)
-
-pnnl.user = "myer056"
-# set working directories
-# Main path is where the raw data will be stored
-#For PC Users:#
-#home.path = paste0("C:/Users/",pnnl.user,"/OneDrive - PNNL/Documents - Core Richland and Sequim Lab-Field Team/Data Generation and Files/")
-#lod.path = paste0(home.path,"Raw_Instrument_Data/IC-6000 MCRL/Data for LOD calculations/")
-
-#for Mac Users:#
-home.path = paste0("/Users/",pnnl.user,"/OneDrive - PNNL/Documents/")
-lod.path = paste0(home.path,"/GitHub/EXCHANGE/Data/LODs")
-
-instrument.path = paste0(home.path,"/GitHub/EXCHANGE/Data/LODs/")
-
-
-write.csv(lod_final, paste0(instrument.path,"ions_LODs_2020_Oct_2022_April_COMPASS_Only.csv"), row.names = FALSE)
-
-
-#### Troubleshooting #####
-
-Why.u.NA <- subset(data_m_sadl,is.na(Slope))
-View(Why.u.NA)
-save(Why.u.NA, file="Mystery_NAs_slope.rda")
-
-
-high <- lod_dates %>% filter(LOD.run > .5)
-View(high)
-write.csv(high, "/Users/myer056/OneDrive - PNNL/Ion Chromatography MSL5-219/LOD_Workup/TroubleLODs_greaterthan0.5ppm.csv")
-
-test <- ggplot()+
-  geom_point(aes(x= Analyte, y= LOD.run, color=Date_Run), data=data_m_sadl)
