e7e4ecaf74d478046360b81c9b0c4cfdf42a0880
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2022-12-07 20:44:23 -0500

    filter standards

--- Processing_Scripts/soil_tctn.R
+++ Processing_Scripts/soil_tctn.R
@@ -39,6 +39,10 @@ directory <- "https://drive.google.com/drive/folders/1OVhQADClTIcfMtbJenoWfCD8fn
 ## Define constants
 f1_min <- 0
 f1_max <- 100
+standard_c <- 71.09
+standard_c_prop <- standard_c * 0.05
+standard_n <- 10.36
+standard_n_prop <- standard_n * 0.05
 
 ## Define analyte
 var <- "TC/TN"
@@ -93,7 +97,9 @@ import_data = function(directory){
 
 data_raw = import_data(directory)
 
-data_raw = data_raw %>% mutate(date_run = str_extract(source, "[0-9]{8}"))
+data_raw = data_raw %>% mutate(
+  date_run = str_extract(source, "[0-9]{1}_[0-9]{2}_[0-9]{4}|[0-9]{2}_[0-9]{2}_[0-9]{4}"),
+  date_run = mdy(str_replace_all(date_run, fixed("_"), "-")))
 
 
 #
@@ -127,36 +133,109 @@ cat("Processing", var, "data...")
 #             lod_tc_average = mean(total_carbon_mg),
 #             lod_tc_sd = sd(total_carbon_mg)) -> lod_processed
 
-data_raw %>% 
-  rename(instrument_id = "...1", sample_id = "...3",
-         total_nitrogen_perc = "Weight\n[%]...9",
-         total_nitrogen_mg = "Weight\n[mg]...8",
-         total_carbon_perc = "Weight\n[%]...16",
-         total_carbon_mg = "Weight\n[mg]...15") %>% 
-  select(instrument_id, sample_id, total_nitrogen_perc, total_nitrogen_mg,
-         total_carbon_perc, total_carbon_mg) %>% 
-  filter(str_detect(instrument_id, "^EC1")) %>% # filter out blanks
-  left_join(key, by = c("instrument_id" = "Original Instrument ID")) %>% 
-  separate('Reassigned Sample ID', into = c("campaign", "kit_id","transect_location", 
-                                            "acidification", "set", "run"), sep = "_") %>% 
-  mutate(transect_location = case_when(transect_location == "WET" ~ "Wetland",
-                                       transect_location == "TRANS" ~ "Transition",
-                                       transect_location == "UPL" ~ "Upland"),
-         acidification = case_when(acidification == "UnAc" ~ FALSE)) %>% 
-  separate(instrument_id, into = c("one", "two", "three", "Month", "Day"), sep = "_") %>% 
-  mutate(Year = "2022", date_ran = make_date(day = Day, month = Month, year = Year)) %>% 
-  bind_cols(lod_processed) -> data_intermediate
+# data_raw %>% 
+#   rename(instrument_id = "...1", sample_id = "...3",
+#          total_nitrogen_perc = "Weight\n[%]...9",
+#          total_nitrogen_mg = "Weight\n[mg]...8",
+#          total_carbon_perc = "Weight\n[%]...16",
+#          total_carbon_mg = "Weight\n[mg]...15") %>% 
+#   select(instrument_id, sample_id, total_nitrogen_perc, total_nitrogen_mg,
+#          total_carbon_perc, total_carbon_mg) %>% 
+#   filter(str_detect(instrument_id, "^EC1")) %>% # filter out blanks
+#   left_join(key, by = c("instrument_id" = "Original Instrument ID")) %>% 
+#   separate('Reassigned Sample ID', into = c("campaign", "kit_id","transect_location", 
+#                                             "acidification", "set", "run"), sep = "_") %>% 
+#   mutate(transect_location = case_when(transect_location == "WET" ~ "Wetland",
+#                                        transect_location == "TRANS" ~ "Transition",
+#                                        transect_location == "UPL" ~ "Upland"),
+#          acidification = case_when(acidification == "UnAc" ~ FALSE)) %>% 
+#   separate(instrument_id, into = c("one", "two", "three", "Month", "Day"), sep = "_") %>% 
+#   mutate(Year = "2022", date_ran = make_date(day = Day, month = Month, year = Year)) %>% 
+#   bind_cols(lod_processed) -> data_intermediate
 
 ## This is where I would first filter to standards and samples dataframes 
 
 cat("Calcuating", var, "data from cal curves...")
 
 #Step 1. filter nitrogen_response and carbon_response and date_ran in standards = Standards Dataframe
+# standards start with STD in sample column
+
+data_raw %>% 
+  filter(grepl("STD", sample)) %>% 
+  mutate(nitrogen_weight_percent = as.numeric(nitrogen_weight_percent),
+         carbon_weight_percent = as.numeric(carbon_weight_percent),
+         nitrogen_response = as.numeric(nitrogen_response),
+         carbon_response = as.numeric(carbon_response)) -> standards_df
+
 #Step 2. filter nitrogen_response and carbon_response and date_ran in samples = Sample Dataframe
+
+data_raw  %>% 
+  filter(!grepl("STD", sample)) %>% 
+  mutate(nitrogen_weight_percent = as.numeric(nitrogen_weight_percent),
+         carbon_weight_percent = as.numeric(carbon_weight_percent),
+         nitrogen_response = as.numeric(nitrogen_response),
+         carbon_response = as.numeric(carbon_response)) -> samples_df
+
 #Step 3. filtering samples outside of range between the response values for 0.1mg to 6mg in Standards Dataframe per date_ran
+
+standards_df %>% 
+  rename(n_response_standards = nitrogen_response, 
+         c_response_standards = carbon_response) %>% 
+  group_by(date_run) %>% 
+  summarise(min_n = min(n_response_standards),
+            max_n = max(n_response_standards),
+            min_c = min(c_response_standards),
+            max_c = max(c_response_standards)) -> standards_range
+
+samples_df %>% 
+  left_join(standards_range, by = "date_run") %>% 
+  filter(nitrogen_response < max_n, nitrogen_response > min_n,
+         carbon_response < max_c, carbon_response > min_c) -> filtered
+
+nrow(data_raw) - nrow(filtered) -> row_change
+cat(row_change, "rows were dropped")
+
 #Step 4. get range of responses from samples in each run (date_ran)
-#Step 5. Filter Standards Dataframe that are reading a weight percentage of C and N that are in line with what the standard should be reporting +/- 5% (Acetanilide_C_per = 71.09, Acetanilide_N_per = 10.36)
-#Step 6. make a linear plot to visually confirm the curve per date_ran is reasonable with good points as is (at min 3 points per curve, ideally as many as possible), no averaging by curve/weight level needed (if curves look wonky, need to average and/or drop points dates)
+
+summary(filtered)
+
+#Step 5. Filter Standards Dataframe that are reading a weight percentage of C and N that are in line 
+# with what the standard should be reporting +/- 5% (Acetanilide_C_per = 71.09, Acetanilide_N_per = 10.36)
+
+# standard_c = 71.09, standard_c_prop = 3.5545
+# standard_n = 10.36, standard_n_prop = 0.518
+standards_df %>% 
+  filter(carbon_weight_percent < standard_c + standard_c_prop,
+         carbon_weight_percent > standard_c - standard_c_prop,
+         nitrogen_weight_percent > standard_n - standard_n_prop,
+         nitrogen_weight_percent < standard_n + standard_n_prop) -> t
+
+nrow(standards_df) - nrow(t) -> row_change_2
+cat(row_change_2, "rows were dropped")
+
+#Step 6. make a linear plot to visually confirm the curve per date_ran is reasonable with good points as is 
+# (at min 3 points per curve, ideally as many as possible), no averaging by curve/weight level needed (if curves look wonky, 
+# need to average and/or drop points dates) x = weight, y = response
+t %>% 
+  group_by(date_run) %>% 
+  summarise(n = n()) -> p
+
+t %>% 
+  ggplot(aes(x = nitrogen_weight_percent, y = nitrogen_response)) + 
+  geom_point() + 
+  geom_text(data = p, aes(label = paste("n =", n)), x = 10.7, y = 1000) +
+  facet_wrap(~date_run) +
+  theme_linedraw() +
+  labs(title = "Nitrogen Standards", x = "Nitrogen Weight (%)", y = "Nitrogen Response")
+
+t %>% 
+  ggplot(aes(x = carbon_weight_percent, y = carbon_response)) + 
+  geom_point() + 
+  geom_text(data = p, aes(label = paste("n =", n)), x = 69, y = 12000) +
+  facet_wrap(~date_run) +
+  theme_linedraw() +
+  labs(title = "Carbon Standards", x = "Carbon Weight (%)", y = "Carbon Response")
+
 #Step 7. Steph talk to Allison & Khadijah
 #Step 8. Response ratios of good/bad curves if needed (@amyerspigg)
 #Step 9. Re-calculate standard curves in R using package EnvStats calibrate function:
