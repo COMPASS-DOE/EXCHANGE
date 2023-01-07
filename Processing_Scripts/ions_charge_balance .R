af6e26549edc384cc04e208a7cb59305c2bf2af9
opalotenburg <opal.otenburg@pnnl.gov>
2023-01-06 19:12:29 -0800

    Update ions_charge_balance .R
    converted to meql

--- Processing_Scripts/ions_charge_balance .R
+++ Processing_Scripts/ions_charge_balance .R
@@ -23,6 +23,8 @@
 ## Allison N Myers-Pigg & Opal Otenburg
 
 library(tidyverse)
+library(googlesheets4)
+library(googledrive)
 
 # Load Data
 
@@ -38,6 +40,18 @@ ion_concs_waters_charge <- cbind(ion_concs_waters,ion_charge)
 
 #meq = mg x # valence electrons / molecular weight 
 
-ion_meq <- ion_concs_waters_charge %>%
+ion_meq <- ion_concs_waters_charge %>% #convert everything to milliequivalents per L
   mutate(nitrate_meql = nitrate_ppm * nitrate_ppm_charge / nitrate_mw,
-         )
+         nitrite_meql = nitrite_ppm * nitrite_ppm_charge / nitrite_mw,
+         fluoride_meql = fluoride_ppm * fluoride_ppm_charge / fluoride_mw,
+         chloride_meql = chloride_ppm * chloride_ppm_charge / chloride_mw,
+         sodium_meql = sodium_ppm * sodium_ppm_charge / sodium_mw,
+         bromide_meql = bromide_ppm * bromide_ppm_charge / bromide_mw,
+         ammonium_meql = ammonium_ppm * ammonium_ppm_charge / ammonium_mw,
+         lithium_meql = lithium_ppm * lithium_ppm_charge / lithium_mw,
+         phosphate_meql = phosphate_ppm * phosphate_ppm_charge / phosphate_mw,
+         potassium_meql = potassium_ppm * potassium_ppm_charge / potassium_mw,
+         sulfate_meql = sulfate_ppm * sulfate_ppm_charge / sulfate_mw,
+         magnesium_meql = magnesium_ppm * magnesium_ppm_charge / magnesium_mw,
+         calcium_meql = calcium_ppm * calcium_ppm_charge / calcium_mw)
+ 
