1b7e37aa2744f12da03b8e719050519dbb5421d1
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2023-03-06 15:50:37 -0500

    need to keep this in lowercase

--- Processing_Scripts/jars_metadata.R
+++ Processing_Scripts/jars_metadata.R
@@ -4,9 +4,9 @@ jars_clean = jars %>%
   mutate(kit_id = stringr::str_extract(Sample,"K[:digit:]{3}" ),
          transect_location = stringr::str_extract(Sample,"upland|transition|wetland|sediment" ),
          campaign = "EC1") %>%
-  mutate(transect_location = stringr::str_replace(transect_location,"upland","Upland"))  %>%
-  mutate(transect_location = stringr::str_replace(transect_location,"transition","Transition"))  %>% 
-  mutate(transect_location = stringr::str_replace(transect_location,"wetland","Wetland")) %>%
-  mutate(transect_location = stringr::str_replace(transect_location,"sediment","Sediment")) %>%
+  # mutate(transect_location = stringr::str_replace(transect_location,"upland","Upland"))  %>%
+  # mutate(transect_location = stringr::str_replace(transect_location,"transition","Transition"))  %>% 
+  # mutate(transect_location = stringr::str_replace(transect_location,"wetland","Wetland")) %>%
+  # mutate(transect_location = stringr::str_replace(transect_location,"sediment","Sediment")) %>%
   rename(sample_weight_g = `Sample weight (from tare) (g)`) %>%
   select(campaign, kit_id, transect_location, sample_weight_g)
