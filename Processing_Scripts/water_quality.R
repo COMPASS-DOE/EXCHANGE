00c8fabd25331700727d55cdc51c829860e40227
amyerspigg <allison.myers-pigg@pnnl.gov>
2023-04-06 11:16:27 -0700

    Update water_quality.R

--- Processing_Scripts/water_quality.R
+++ Processing_Scripts/water_quality.R
@@ -39,7 +39,7 @@ titrator_path = "https://docs.google.com/spreadsheets/d/1lP4ft29oknaR5Xthv3Qo29E
 ##
 ## Salinity is calculated from spcond using the GSW toolbox
 ## Eh is calculated from orp_mv using Eh = orp_mv + 207, per sensor manual
-water_quality_raw <- read_sheet(titrator_path, col_types = 'Tcddddddc') %>% 
+water_quality_primitive <- read_sheet(titrator_path, col_types = 'Tcddddddc') %>% 
   mutate(campaign = "EC1",
          transect_location = "Water",
          sal_psu = gsw_SP_from_C(C = spcond_mscm, t = 25, p = 0), 
@@ -55,21 +55,34 @@ clean_data <- function(data) {
     mutate(sal_psu = round(sal_psu, 2), 
            ph = round(ph, 2), 
            orp_mv = round(orp_mv, 0), 
-           alk_mgl_caco3 = round(alk_mgl_caco3, 0)) %>% 
+           alk_mgl_caco3 = round(alk_mgl_caco3, 0),
+           transect_location = tolower(transect_location)) %>% 
     ## Second, add flags for 
-    mutate(sal_flag = ifelse(sal_psu < 0 | sal_psu > 50, "outside range", "NA"), 
-           ph_flag = ifelse(ph < 0 | ph > 14, "outside range", "NA"), 
-           orp_flag = ifelse(orp_mv < 0 | orp_mv > 500, "outside range", "NA"),
-           alk_flag = ifelse(alk_mgl_caco3 < 0, "outside range", "NA"))
+    mutate(sal_flag = ifelse(sal_psu < 0 | sal_psu > 50, "outside range", NA), 
+           ph_flag = ifelse(ph < 0 | ph > 14, "outside range", NA), 
+           orp_flag = ifelse(orp_mv < 0 | orp_mv > 500, "outside range", NA),
+           alk_flag = ifelse(alk_mgl_caco3 < 0, "outside range", NA))
 }
 
-water_quality <- clean_data(water_quality_raw) %>% 
-  drop_na() # drop NA columns
+water_quality <- clean_data(water_quality_primitive) 
+  
 
 
 # 4. Write L0B data to Google Drive ---------------------------------------------
-date_updated <- "20220509"
+water_quality %>% write.csv(paste0("./ec1_water_waterquality_l0B_", Sys.Date(), ".csv"), row.names = FALSE)
 
-write_csv(water_quality, paste0("Data/Processed/EC1_Water_WaterQuality_L0B_", date_updated, ".csv"))
+L0Bdirectory = "https://drive.google.com/drive/folders/1yhukHvW4kCp6mN2jvcqmtq3XA5niKVR3"
 
+drive_upload(media = paste0("./ec1_water_waterquality_l0B_", Sys.Date(), ".csv"), name= paste0("ec1_soil_gwc_l0B_", Sys.Date(), ".csv"), path = L0Bdirectory)
 
+file.remove(paste0("./ec1_water_waterquality_l0B_", Sys.Date(), ".csv"))
+
+# 5. Check with Metadata for missing:
+
+source("./Processing_Scripts/Metadata_kit_list.R")
+
+metadata_collected %>%
+  filter(sample_method == "bottle_125ml")-> meta_filter
+
+check = water_quality %>%
+  full_join(meta_filter, by = c("campaign", "kit_id", "transect_location")) 
