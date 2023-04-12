3a0211ce8206e5ac86b4adcde591bce3f9813e2b
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2023-04-12 14:27:29 -0400

    Add low range flag for mS

--- Processing_Scripts/soil_pH_conductivity.R
+++ Processing_Scripts/soil_pH_conductivity.R
@@ -72,22 +72,10 @@ soil_pH_qc =
          specific_conductance_flag = case_when(specific_conductance_us_cm < 0 ~ "below range",
                                                !grepl("measured as milliSiemens", Notes) & specific_conductance_us_cm > 9999 ~ "above range",
                                                grepl("measured as milliSiemens", Notes) & specific_conductance_us_cm > 200000 ~ "above range")) %>% 
-  dplyr::select(-starts_with("ph"), -starts_with("specific_conductance"), starts_with("ph"), starts_with("specific_conductance"), -Notes) %>% 
+  dplyr::select(campaign, kit_id, transect_location, ph, specific_conductance_us_cm, ph_flag, specific_conductance_flag) %>% 
   janitor::clean_names()
-  
 
-#
-# 5. Export cleaned data --------------------------------------------------
-
-soil_pH_qc %>% write.csv(paste0("./ec1_soil_ph_cond_l0B_", Sys.Date(), ".csv"), row.names = FALSE)
-
-L0Bdirectory = "https://drive.google.com/drive/folders/1yhukHvW4kCp6mN2jvcqmtq3XA5niKVR3"
-
-drive_upload(media = paste0("./ec1_soil_ph_cond_l0B_", Sys.Date(), ".csv"), name= paste0("ec1_soil_ph_cond_l0B_", Sys.Date(), ".csv"), path = L0Bdirectory )
-
-file.remove(paste0("./ec1_soil_ph_cond_l0B_", Sys.Date(), ".csv"))
-
-# 6. Check with Metadata for missing:
+# 5. Check with Metadata for missing:
 
 source("./Processing_Scripts/Metadata_kit_list.R")
 source("./Processing_Scripts/jars_metadata.R")
@@ -97,5 +85,20 @@ metadata_collected %>%
 
 soil_pH_qc %>% 
   full_join(meta_filter, by = c("campaign", "kit_id", "transect_location")) %>% 
-  full_join(jars_clean, by = c("campaign", "kit_id", "transect_location")) %>% 
-  filter(collected == TRUE & is.na(ph) | collected == FALSE & !is.na(ph)) -> check_these
+  full_join(jars_clean, by = c("campaign", "kit_id", "transect_location"))  %>% 
+  filter(transect_location != "sediment") %>% 
+  mutate(ph_flag = case_when(is.na(ph) & collected == FALSE ~ "sample not collected",
+                             TRUE ~ ph_flag),
+         specific_conductance_flag = case_when(is.na(specific_conductance_us_cm) & collected == FALSE ~ "sample not collected",
+                             TRUE ~ specific_conductance_flag)) -> soil_ph_cond_full
+
+#
+# 6. Export cleaned data --------------------------------------------------
+
+soil_ph_cond_full %>% write.csv(paste0("./ec1_soil_ph_cond_L1_", Sys.Date(), ".csv"), row.names = FALSE)
+
+L1directory = "https://drive.google.com/drive/folders/1yhukHvW4kCp6mN2jvcqmtq3XA5niKVR3"
+
+drive_upload(media = paste0("./ec1_soil_ph_cond_L1_", Sys.Date(), ".csv"), name= paste0("ec1_soil_ph_cond_L1_", Sys.Date(), ".csv"), path = L1directory )
+
+file.remove(paste0("./ec1_soil_ph_cond_L1_", Sys.Date(), ".csv"))
