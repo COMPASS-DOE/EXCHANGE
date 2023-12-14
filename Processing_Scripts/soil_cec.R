5d3e1bfc3735120d39bd807e1af629124121bbe8
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2023-12-13 16:54:46 -0800

    clean up, and do cec calculations

--- Processing_Scripts/soil_cec.R
+++ Processing_Scripts/soil_cec.R
@@ -70,7 +70,9 @@ sample_key = read.csv("Data/EC1_Metadata_KitLevel.csv")
 ## 1. applying dilution correction
 ## 2. normalizing to soil mass (ug/g)
 ## 3. applying flags for analytes that were below detect
+## 4. calculating CEC (in meq/100g)
 
+## 4a. normalize to soil mass ----
 processed_data = 
   raw_data %>% 
   mutate_at(vars(-c(Sample)), as.numeric) %>% 
@@ -96,16 +98,16 @@ samples =
   dplyr::select(-Sample, -note) %>% 
   mutate(mgL_corr = round(mgL_corr, 2),
          mgg = mgL * (vol_extractant_mL / wt_soil_g) * (1/1000),
-         ugg = mgg * 1000, 
-         ugg = signif(ugg, 5),
-         #ugg = formatC(ugg, digits = 5)
-         ) %>% 
+         ug_g = mgg * 1000, 
+         ug_g = signif(ug_g, 5),
+         #ug_g = formatC(ug_g, digits = 5)
+  ) %>% 
   mutate(skip = (analysis_ID == "EC1_ICP_137" & element == "K")) %>% 
   filter(!skip) %>% 
-  dplyr::select(analysis_ID, kit_id, transect, element, ugg) 
+  dplyr::select(analysis_ID, kit_id, transect, element, mgL, ug_g) 
 
-
-
+#
+## 4b. set flags ----
 # set a flag for all the elements that are below detection
 # some samples have one, some samples have multiple. 
 # setting individual flags and then concatenate using `toString()`
@@ -120,27 +122,73 @@ samples_flag =
                    flag = paste(flag, "below detect")) %>% 
   ungroup()
 
+#
+## 4c. finalize dataset for ug/g ----
 samples_wide = 
   samples %>% 
-  mutate(element = paste0(element, "_ugg")) %>% 
-  pivot_wider(names_from = "element", values_from = "ugg") %>% 
+  dplyr::select(-mgL) %>% 
+  mutate(element = paste0(element, "_ug_g")) %>% 
+  pivot_wider(names_from = "element", values_from = "ug_g") %>% 
   left_join(samples_flag) %>% 
   dplyr::select(-analysis_ID)
 
-# join sample key
-samples_with_key = 
-  samples_wide %>% 
-  left_join(sample_key %>% dplyr::select(kit_id, region))
+#
+## 4b. CEC calculations ----
+## CEC is calculated in terms of meq/100g
+## for this we need charge and atomic weight per cation 
 
-samples %>% 
-  left_join(sample_key %>% dplyr::select(kit_id, region)) %>% 
-  ggplot(aes(x = region, y = ugg, color = region))+
-  geom_jitter()+
-  facet_wrap(~element, scales = "free")
+charges = 
+  tribble(
+    ~element, ~charge, ~atomic_wt,
+    "Na", 1, 23,
+    "K", 1, 39,
+    "Ca", 2, 40,
+    "Mg", 2, 24,
+    "Al", 3, 27
+  )
+
+samples_meq = 
+  samples %>% 
+  dplyr::select(analysis_ID, kit_id, transect, element, ug_g) %>% 
+  left_join(charges) %>% 
+  #drop_na() %>% 
+  mutate(#ion = paste0(ion, "_meq_100g"), 
+    meq_100g = ug_g * charge/atomic_wt * 100/1000,
+    meq_100g = round(meq_100g, 2)) %>% 
+  filter(!is.na(meq_100g)) %>% 
+  group_by(analysis_ID) %>% 
+  #dplyr::mutate(CEC_meq_100g = sum(meq_100g)) %>% 
+  pivot_longer(cols = c(ug_g, meq_100g)) %>% 
+  mutate(element = paste0(element, "_", name)) 
+
+cec = 
+  samples_meq %>% 
+  filter(name == "meq_100g") %>% 
+  group_by(analysis_ID) %>% 
+  dplyr::summarise(CEC_meq_100g = sum(value))
+
+cations_and_cec = 
+  samples_meq %>% 
+  dplyr::select(-c(charge, atomic_wt, name)) %>% 
+  pivot_wider(names_from = "element", values_from = "value") %>% 
+  dplyr::select(analysis_ID, kit_id, transect, ends_with("meq_100g")) %>% 
+  left_join(cec) %>% 
+  left_join(samples_flag)
+
 
 #
 # 11. Write L0B data -----------------------------------------------------------
 
-write_csv(samples_wide, paste0("Data/Processed/EC1_Soil_ICP_L0B_", Sys.Date(), ".csv"))
+write_csv(samples_wide, paste0("Data/Processed/EC1_Soil_ICP_ugg_L0B_", Sys.Date(), ".csv"))
+write_csv(cations_and_cec, paste0("Data/Processed/EC1_Soil_ICP_CEC_L0B_", Sys.Date(), ".csv"))
 
 
+
+
+# testing
+# join sample key
+samples %>% 
+  left_join(sample_key %>% dplyr::select(kit_id, region)) %>% 
+  ggplot(aes(x = region, y = ug_g, color = region))+
+  geom_jitter()+
+  facet_wrap(~element, scales = "free")
