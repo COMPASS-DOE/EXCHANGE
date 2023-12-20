f5d4907fd05620ae36dc5132972353ee06ba840f
amyerspigg <allison.myers-pigg@pnnl.gov>
2023-12-20 09:50:27 -0800

    embarking again on ions journey

--- Processing_Scripts/water_ions_v2.R
+++ Processing_Scripts/water_ions_v2.R
@@ -106,13 +106,13 @@ import_data = function(directory){
       #  df <- read.delim(path, skip = 2)
       df[["source"]] <- rep(path, nrow(df))
       df}))
-
+  
   ## d. delete the temporary files
   file.remove(c(files$name))  
   
   ## e. output
   dat
-#  browser()
+  #  browser()
 }
 
 ## import the raw data files
@@ -220,24 +220,24 @@ process_data = function(raw_data, readme_data, IONS){
     # therefore, make sure to include ALL ion names in the IONS parameter
     fill(Ion) %>% 
     dplyr::select(-Row_number, -label)
- 
+  
   # the dataframe now has all the ions assigned to each row
   # but it is still horribly untidy
   
   # b. clean the dataframe -----
-      
-      ### create header by collapsing the header + first row
-      ### new_header = 
-      ###   data_new %>% 
-      ###   colnames() %>% 
-      ###   paste0(data_new[1,]) %>% 
-      ###   str_remove_all("NA")
-      ### # the "source" column has "source" with a lot more crap
-      ### # use grepl to replace that full value with just "source"
-      ### new_header = replace(new_header, grep("source", new_header), "source")
-      ##
-      ### # set column names for the dataframe
-      ### names(data_new) <- new_header  
+  
+  ### create header by collapsing the header + first row
+  ### new_header = 
+  ###   data_new %>% 
+  ###   colnames() %>% 
+  ###   paste0(data_new[1,]) %>% 
+  ###   str_remove_all("NA")
+  ### # the "source" column has "source" with a lot more crap
+  ### # use grepl to replace that full value with just "source"
+  ### new_header = replace(new_header, grep("source", new_header), "source")
+  ##
+  ### # set column names for the dataframe
+  ### names(data_new) <- new_header  
   
   # preliminary processing to make it tidy
   data_new_processed = 
@@ -279,14 +279,14 @@ process_data = function(raw_data, readme_data, IONS){
     #  filter(!Action %in% "Omit") %>%
     filter(is.na(REMOVE), Name != "Name") %>% 
     dplyr::select(-REMOVE, -Action_original)
- 
+  
   data_new_processed_readme
   # This file has all the processed data for all the samples/standards/blanks run on the machine.
   # Some samples were run multiple times, at varying dilutions
   # We will use the Readme file to map these dilutions later, 
   # and then pick only the dilutions we want.
   # --> see the `do_corrections()` function
-
+  
 }
 
 # Now, run the function
@@ -415,15 +415,15 @@ calculate_lods = function(data_ions_processed, z, IONS, directory_slope){
   
   data_m_sadl
   #save(data_m_sadl, file="LODs_byrun_COMPASS.rda")
-
+  
   
 }
 
 #Calculate LODs: 
 ions_lods = calculate_lods(data_ions_processed, 
-                          z = 3, 
-                          IONS = all_ions,
-                          directory_slope = "https://drive.google.com/drive/u/1/folders/1pf5oxzg15uB0twTl76sEGD0Ayan8ckBy")
+                           z = 3, 
+                           IONS = all_ions,
+                           directory_slope = "https://drive.google.com/drive/u/1/folders/1pf5oxzg15uB0twTl76sEGD0Ayan8ckBy")
 
 
 ## 4b. `apply_qc_flags`: applying QC flags -------------------------------------
@@ -451,224 +451,9 @@ apply_qc_flags = function(data_ions_processed, QC_DATA){
                             TRUE ~ flag)) %>% 
     rename(ppm = Amount) %>% 
     dplyr::select(Name, date_run, Ion, ppm, flag, Action, Dilution)
-
+  
 }
 
 #Run Function: 
 data_ions_qc = apply_qc_flags(data_ions_processed, QC_DATA = ions_lods)
 
-
-## 4c. Do dilution/blank corrections ----------------------------------------
-
-# `do_corrections`: this function will apply blank and dilution corrections
-# input parameters are:
-#  (a) the processed_data dataframe with qc flags (data_ions_qc), 
-#  (b) compiled readme file, which contains data for dilutions, etc.,
-#  (c) and the dilutions key, which tells us which dilutions we want to keep for each sample/ion
-
-#Create Function
-do_corrections = function(data_ions_qc, dilutions_key){
-  # 1. blank corrections ----
-  samples_and_blanks = 
-    data_ions_qc %>% 
-    filter(grepl("EC1_", Name) | grepl("Blank", Name)) %>% 
-    filter(!Name %in% c("Blank1", "Blank2", "Blank3", "Blank4")) %>% #filter out conditioning blanks & blanks after curve
-    filter(!grepl("CondBlank", Name)) %>% 
-    # assign sample or blank
-    mutate(sample_type = case_when(grepl("Blank", Name) ~ "Blank",
-                                   grepl("EC1_", Name) ~ "Sample")) 
-  
-  blank_mean = #Mean of blanks for each run 
-    samples_and_blanks %>% 
-    filter(sample_type == "Blank") %>% 
-    group_by(Ion, date_run) %>% 
-    dplyr::summarise(blank_mean_ppm = mean(ppm, na.rm = TRUE)) 
-    
-  samples_blank_corrected = 
-    samples_and_blanks %>% 
-    filter(sample_type == "Sample") %>% 
-    left_join(blank_mean, by = c("Ion", "date_run")) %>% 
-    mutate(blank_mean_ppm = replace_na(blank_mean_ppm,0),
-           ppm= case_when(grepl("below limit of detection", flag) ~ NA_real_,
-                          TRUE ~ ppm)) %>% 
-    mutate(Amount_bl_corrected = ppm - blank_mean_ppm) %>%
-    mutate(Amount_bl_corrected = case_when(Amount_bl_corrected <= 0 ~ NA_real_, 
-                                     TRUE ~ Amount_bl_corrected),
-   flag = case_when(is.na(Amount_bl_corrected) & is.na(ppm) ~ flag,
-                          is.na(Amount_bl_corrected)  & !is.na(ppm) ~ "below blank",
-                    is.na(flag) & grepl("blank correct", Action) ~ "blank corrected",
-   TRUE ~ flag))
-    
-  
-  # # WHYYYY
-  # # 2. dilution correction ----
-   options(scipen = 50)
-  # isolate the ones run at the same dilution level multiple times
-   samples_dilution_corrected_problem_children = 
-     samples_blank_corrected %>% 
-  #   # bring in the dilutions key to determine which dilutions to keep for which ion
-     right_join(dilutions_key, by = c("Name" = "kit_id", "Ion" = "Ion", "Dilution" = "dilution", "date_run")) %>% 
-     group_by(Name, Ion, Dilution) %>% 
-     filter(n()>1) %>% 
-     mutate(max = max(Amount_bl_corrected),
-            min = min(Amount_bl_corrected),
-            mean = mean(Amount_bl_corrected),
-            percent_diff = ((((max - min) / mean) * 100 )),
-            check = if_else(percent_diff > 5 | is.na(ppm), "FLAG", as.character(mean)),
-            keep = if_else(date_run == max(date_run) & check == "FLAG" | !is.na(Amount_bl_corrected), as.character(Amount_bl_corrected), check),
-            flag_reps = if_else(duplicated(keep, fromLast =TRUE, incomparables = NA) | duplicated(keep, incomparables = NA), "replicates averaged", "NA" )) %>%
-     filter(is.na(keep) | keep != "FLAG") %>%
-     distinct(Name, Ion, Dilution, keep, .keep_all = TRUE) %>%
-     unite(col = "flag", c("flag", "flag_reps"), sep= ", ", na.rm = TRUE, remove = TRUE) %>%
-     select(Name, date_run, Ion, Dilution, flag, keep) %>%
-     rename(Amount_bl_corrected = keep) %>%
-     mutate(flag =na_if(flag, "NA"),
-            Amount_bl_corrected = as.numeric(Amount_bl_corrected))
-   
-   
-   # value = if_else(check == "FLAG" & keep == "NO", Amount_bl_corrected, mean)
-   
-   samples_dilution_corrected_unique = 
-     samples_blank_corrected %>% 
-     #   # bring in the dilutions key to determine which dilutions to keep for which ion
-     right_join(dilutions_key, by = c("Name" = "kit_id", "Ion" = "Ion", "Dilution" = "dilution", "date_run")) %>% 
-     group_by(Name, Ion, Dilution) %>% 
-     filter(n()==1) %>%
-     select(Name, date_run, Ion, Dilution,flag, Amount_bl_corrected)
-   
-
-   samples_blank_corrected_dups_corrected = bind_rows(samples_dilution_corrected_problem_children, samples_dilution_corrected_unique)  
-     
-  #   # do the dilution correction
-   samples_dilution_corrected = samples_blank_corrected_dups_corrected %>%
-     mutate(Amount_bl_dil_corrected = Amount_bl_corrected * Dilution) %>% 
-     mutate(Amount_bl_dil_corrected = as.numeric(Amount_bl_dil_corrected),
-            Amount_bl_dil_corrected = round(Amount_bl_dil_corrected, 3),
-            flag_dilution = case_when(Dilution > 1 ~ "dilution corrected")) %>% 
-     dplyr::select(Name, date_run, Ion, Amount_bl_dil_corrected, flag, flag_dilution, Dilution) %>%
-     unite(col = "flag", c("flag", "flag_dilution"), sep= ", ", na.rm = TRUE, remove = TRUE)
-  # 
-  # samples_dilution_corrected_ALLDILUTIONS = 
-  #    samples_blank_corrected %>% 
-  # #   # bring in the dilutions key to determine which dilutions to keep
-  #   left_join(dilutions_key, by = c("Name" = "kit_id", "Ion" = "Ion", "Dilution" = "dilution", "date_run")) %>% 
-  # #   # do the dilution correction
-  #    mutate(Amount_bl_dil_corrected = Amount_bl_corrected * Dilution) %>% 
-  #    mutate(Amount_bl_dil_corrected = as.numeric(Amount_bl_dil_corrected),
-  #           Amount_bl_dil_corrected = round(Amount_bl_dil_corrected, 3)) %>% 
-  #    dplyr::select(Name, date_run, Ion, Amount_bl_dil_corrected, flag, Action, Dilution) 
-  # # 
-  # # 
-  # # 
-  #  list(samples_dilution_corrected = samples_dilution_corrected,
-  #       samples_dilution_corrected_ALLDILUTIONS = samples_dilution_corrected_ALLDILUTIONS
-  # )
-
-}
-
-#Run Function:
-data_ions_corrected = do_corrections(data_ions_qc, dilutions_key)
-
-
-## checking dilution issues #####
-
-dilution_level_check = data_ions_corrected %>%
-  filter(grepl("below instrument detection, dilution corrected", flag) & is.na(Amount_bl_dil_corrected)) %>%
-  mutate(used = "yes") %>%
-  group_by(Name, Ion) 
-
-all_dilution_level_check = data_ions_qc %>%
-  select(Name, date_run, Ion, Dilution, flag)
-
-check_dil_level = all_dilution_level_check %>%
-  group_by(Name, Ion) %>%
-  right_join(select(dilution_level_check, Name, Ion)) %>%
-  full_join(dilution_level_check, by= c("Name","Ion", "Dilution", "date_run")) %>%
-  arrange(Name, Ion) %>%
-  select(-Amount_bl_dil_corrected)
-
-
-#data_ions_corrected_all_dilutions = do_corrections(data_ions_qc, dilutions_key)$samples_dilution_corrected_ALLDILUTIONS
-
-
-# 5. final formatting ----------------------------------------------------
-
-# `format_df`: format the dataframe to a more legible format in wideform, with a flag column for each ion
-
-#Create Function:
-format_df = function(data_ions_corrected){
-  data_ions_corrected %>% 
-    ungroup() %>% 
-    rename(ppm = Amount_bl_dil_corrected) %>% 
-     mutate(ppm = as.character(ppm),
-            Dilution = as.character(Dilution)) %>%
-    pivot_longer(-c(Name, date_run, Ion)) %>% 
-    mutate(name2 = paste0(Ion, "_", name)) %>% 
-    dplyr::select(-Ion, -name, -date_run) %>% 
-    pivot_wider(names_from = "name2", values_from = "value") %>% 
-    separate(Name, sep = "_", into = c("campaign", "kit_id")) %>% 
-    mutate(transect_location = "Water") %>% 
-    dplyr::select(campaign, kit_id, transect_location, everything()) %>% 
-    mutate(across(ends_with("_ppm"), as.numeric)) %>% 
-    janitor::clean_names() %>% 
-   # mutate(bromide_flag = case_when(bromide_ppm == NA ~ "below detect"))
-    
-    #mutate(across(ends_with("_flag"), 
-  #          case_when(ends_with("_ppm") == NA ~ "below detect"))) %>% #COME BACK HERE TO FIX NAs introduced by not detecting
-    #maybe try this with stringr instead of ends_with???
-    arrange(kit_id)
-  
-  
-}
-
-#Run Function: 
-data_ions_final = format_df(data_ions_corrected) # this includes only the results for the selected (correct) dilutions
-
-
-#data_ions_final_all_dilutions = format_df(data_ions_corrected_all_dilutions) # this includes results for all dilutions (including the ones we want to exclude)
-
-
-#
-# 5. Export cleaned data --------------------------------------------------
-
-data_ions_final %>% write.csv("Data/Processed/L0B/EC1_Water_Ions_L0B_TEMP_WITH_dilutions_03092023.csv", row.names = FALSE)
-data_ions_final_all_dilutions %>% write.csv("Data/Processed/L0B/EC1_Water_Ions_L0B_20221202_WITH_ALL_dilutions.csv", row.names = FALSE)
-  
-#
-# xx. other functions -----------------------------------------------------
-
-check_cal_curve_values = function(){
-  
-  ## (side code) the lower end of cal curves is generally NA. 
-  ## check if samples have values below the non-NA cal curves.
-  
-  # first, pull out all the standards
-  data_standards = 
-    data_ions_processed %>% 
-    filter(grepl("A-", Name) | grepl("C-", Name)) %>% 
-    filter(!grepl("CK", Name)) %>%
-    filter(!is.na(Amount)) %>% 
-    group_by(Ion, date_run) %>% 
-    dplyr::summarise(amount_min = min(Amount))
-  
-  data_samples = 
-    data_ions_processed %>% 
-    filter(grepl("EC1_", Name))  %>% 
-    dplyr::select(Name, Ion, date_run, Amount) %>% 
-    left_join(data_standards) %>% 
-    mutate(less_than_cal = Amount < amount_min)
-}
-
-
-# Plottings 
-a = data_ions_final_all_dilutions %>%
-  ggplot() +
-  geom_point(aes(kit_id, as.numeric(nitrate_ppm)))
-
-  ggplotly(a)
-  
-#QC stuff
-data_ions_qc %>% write.csv("TEMP-EC1-ions-not-dilution-corrected_2022-10-12.csv", row.names = FALSE)
-file.remove("TEMP-ions_readme_compiled_2022-10-12.csv")
-
-file.remove("TEMP-EC1-ions-not-dilution-corrected_2022-10-12.csv")
