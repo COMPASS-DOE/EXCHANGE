ab7aa6dd3fb787cfa31bf3b87e1c17e1fccd8a94
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2024-02-28 14:20:21 -0500

    save to L1 directory

--- Processing_Scripts/soilsediment_visual_metrics.R
+++ Processing_Scripts/soilsediment_visual_metrics.R
@@ -130,11 +130,16 @@ data_clean %>%
                                    TRUE ~ glass_plastic),
          white_flakes = case_when(notes == "kit compromised" ~ NA,
                                   notes == "sample compromised" ~ NA,
-                                  TRUE ~ white_flakes)) -> test
+                                  TRUE ~ white_flakes)) -> soilviz_full
 
 # 6. Write cleaned data to drive -----------------------------------------------
 
-## We should add Sys.date or hardcode date so we know when the L0B was born
-## The file written out should be named following 
-## [Campaign]_[Analyte]_[QC_level]_[Date_of_creation_YYYYMMDD].csv
-#drive_upload(media = data_clean, path = data_path)
+
+soilviz_full %>% write.csv(paste0("./ec1_soilsediment_visualmetrics_L1_", Sys.Date(), ".csv"), row.names = FALSE)
+
+L1directory = "https://drive.google.com/drive/folders/1yhukHvW4kCp6mN2jvcqmtq3XA5niKVR3"
+
+drive_upload(media = paste0("./ec1_soilsediment_visualmetrics_L1_", Sys.Date(), ".csv"), name = paste0("ec1_soilsediment_visualmetrics_L1_", Sys.Date(), ".csv"), path = L1directory)
+
+file.remove(paste0("./ec1_soilsediment_visualmetrics_L1_", Sys.Date(), ".csv"))
+
