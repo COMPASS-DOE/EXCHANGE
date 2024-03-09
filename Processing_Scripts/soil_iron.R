d6834a6f9f9974a528cc31e2c6e42641fb7e7111
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2024-03-09 10:46:33 -0800

    general cleaning. update calibration function

--- Processing_Scripts/soil_iron.R
+++ Processing_Scripts/soil_iron.R
@@ -75,18 +75,20 @@ raw_data = import_data(directory)
 
 map_processed = 
   ferrozine_map %>% 
-  fill(plate, analysis, notes) %>% 
-  mutate(plate = parse_number(plate)) %>% 
-  pivot_longer(-c(plate, notes, letter, analysis), names_to = "number", values_to = "sample_label") %>% 
+  fill(date, plate, analysis, notes) %>% 
+  mutate(plate = parse_number(plate),
+         date = ymd(date)) %>% 
+  pivot_longer(-c(date, plate, notes, dilution, letter, analysis), names_to = "number", values_to = "sample_label") %>% 
   drop_na() %>% 
   filter(letter != "letter") %>% 
-  mutate(sample_type = case_when(grepl("ppm", sample_label) ~ "standard",
+  mutate(sample_type = case_when(grepl("mM", sample_label) ~ "standard",
                                  grepl("blank", sample_label, ignore.case = T) ~ "blank",
                                  grepl("K[0-9]", sample_label) ~"sample"),
          well_position = paste0(letter, number)) %>% 
   rename(tray_number = plate) %>% 
-  dplyr::select(tray_number, well_position, sample_label, sample_type, notes) %>% 
-  mutate(standard_ppm = case_when(sample_type == "standard" ~ parse_number(sample_label)))
+  dplyr::select(date, tray_number, well_position, sample_label, sample_type, dilution) %>% 
+#  mutate(standard_mM = case_when(sample_type == "standard" ~ parse_number(sample_label))) %>% 
+  force()
 
 data_processed = 
   raw_data %>% 
@@ -98,32 +100,51 @@ data_processed =
   pivot_longer(-c(source, x_1), values_to = "absorbance_562") %>% 
   mutate(name = str_remove(name, "x"),
          well_position = paste0(x_1, name),
+         date = str_extract(source, "[0-9]{8}"),
+         date = ymd(date),
          tray_number = str_extract(source, "tray|plate[1-9]+"),
          tray_number = parse_number(tray_number),
          absorbance_562 = as.numeric(absorbance_562)) %>% 
-  dplyr::select(tray_number, well_position, absorbance_562) %>% 
-  right_join(map_processed, by = c("tray_number", "well_position")) %>% 
-  filter(!notes %in% "skip")
+  dplyr::select(date, tray_number, well_position, absorbance_562) %>% 
+  right_join(map_processed, by = c("date", "tray_number", "well_position")) 
 
 #
 # 4. Apply calibrations ---------------------------------------------------
+
 calibrate_ferrozine_data = function(data_processed){
+  # now do the calibrations
+  # standards are in mM
+  # molecular formula for FAS = (NH₄)₂Fe(SO₄)₂·6H₂O
+  # so 1 M FAS = 1M Fe
+  # 1 mM FAS = 1 * 55.85 mg Fe in 1 L solution = 55.85 mg Fe in 1 L solution
+  # therefore 1 mM = 55.85 mg/L or 55.85 ppm
+  
   standards = 
     data_processed %>% 
-    filter(grepl("standard", sample_type)) %>% 
-    dplyr::select(tray_number, absorbance_562, standard_ppm) %>% 
-    mutate(standard_ppm = as.numeric(standard_ppm))
+    filter(sample_type == "standard") %>% 
+    mutate(standard_mM = parse_number(sample_label),
+           standard_type = str_extract(sample_label, "FAS|FeCl3"),
+           standard_ppm =  case_when(standard_type == "FAS" ~ standard_mM * 55.85)) %>% 
+    #dplyr::select(date, tray, absorbance_562, standard_ppm) %>% 
+    mutate(standard_ppm = as.numeric(standard_ppm)) %>% 
+    dplyr::filter(standard_type == "FAS") %>% 
+    # the curve is linear only below 2 mM
+    filter(standard_mM <= 2)
   
-  standards %>% 
+  gg_calibration = 
+    standards %>% 
+    filter(standard_mM <= 2) %>% 
     ggplot(aes(x = standard_ppm, y = absorbance_562, color = as.character(tray_number)))+
     geom_point()+
     geom_smooth(method = "lm", se = F)+
-    facet_wrap(~tray_number)
+    facet_wrap(~date + tray_number)
   
-  # using only tray2 for calibration because tray1 wasn't good
   calibration_coef = 
     standards %>% 
-    filter(tray_number == 2) %>% 
+    # 2022-12-22 tray 1 cc is bad, so drop
+    filter(!(date == "2022-12-22" & tray_number == 1)) %>% 
+    drop_na() %>% 
+    dplyr::group_by(date) %>% 
     dplyr::summarize(slope = lm(absorbance_562 ~ standard_ppm)$coefficients["standard_ppm"], 
                      intercept = lm(absorbance_562 ~ standard_ppm)$coefficients["(Intercept)"])
   
@@ -131,16 +152,23 @@ calibrate_ferrozine_data = function(data_processed){
   # abs = m*ppm + c
   # ppm = abs-c/m
   
-  data_processed %>% 
-    cross_join(calibration_coef) %>% 
+  data_calibrated = 
+    data_processed %>% 
+    left_join(calibration_coef, by = c("date")) %>% 
     mutate(ppm_calculated = ((absorbance_562 - intercept) / slope))
   
+  list(calibration_coef = calibration_coef,
+       data_calibrated = data_calibrated,
+       gg_calibration = gg_calibration)
 }
+
+calibration_curves = calibrate_ferrozine_data(data_processed)$gg_calibration
+
 
 samples = 
   calibrate_ferrozine_data(data_processed) %>% 
   filter(sample_type == "sample") %>% 
-  dplyr::select(sample_label, ppm_calculated, notes) %>% 
+  dplyr::select(sample_label, ppm_calculated, dilution) %>% 
   mutate(dilution = case_when(grepl("diluted 2x", notes) ~ 2,
                               TRUE ~ 1),
          # all samples were diluted 2x with HCl, and then some were diluted again
