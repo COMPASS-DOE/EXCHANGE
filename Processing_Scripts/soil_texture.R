59076dac5b18b9d108db31f952b00cf81b7c53e6
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2024-11-13 19:29:48 -0800

    move script to template

--- Processing_Scripts/soil_texture.R
+++ Processing_Scripts/soil_texture.R
@@ -1,11 +1,41 @@
-library(tidyverse)
-library(googlesheets4)
+## This script imports raw data for soil texture measured using the hydrometer method.
+## Data are read in from Google Drive
+## 
+## Created: 2024-11-13
+## Kaizad F. Patel
+##
 
-texture_data = read_sheet("https://docs.google.com/spreadsheets/d/1PSKKbnmr3jVhjv172mPynmkt3mVlOH_5KQgSa7DTCIU/edit?gid=0#gid=0", col_types = "c")
-texture_data2 = 
-  texture_data %>% 
-  mutate_at(vars(-c(sample_id, date_started, notes)), as.numeric)
+# 1. Setup ---------------------------------------------------------------------
 
+# load packages
+require(pacman)
+pacman::p_load(cowsay,
+               tidyverse, # keep things tidy
+               janitor, # useful for simplifying column names
+               googlesheets4 # read_sheet 
+               )
+
+## Welcome
+say("Welcome to EXCHANGE!", by = "random")
+
+## Define analyte
+var <- "soil texture"
+
+
+## URL for data
+data_path = "https://docs.google.com/spreadsheets/d/1PSKKbnmr3jVhjv172mPynmkt3mVlOH_5KQgSa7DTCIU"
+
+#
+# 2. Import data ---------------------------------------------------------------
+cat("Importing", var, "data...")
+
+## read in raw data
+data_raw <- googlesheets4::read_sheet(data_path, col_types = "c")
+
+#
+# 3. Process data ---------------------------------------------------------
+
+# function to compute % sand-silt-clay from the hydrometer readings
 compute_soil_texture = function(dat){
   
   dat %>% 
@@ -39,10 +69,14 @@ compute_soil_texture = function(dat){
       percent_sand = ((wt_sieve_soil_53um_g - wt_sieve_g)/wt_dry_soil_g) * 100,
       percent_silt = 100 - (percent_sand + percent_clay)
     ) %>% 
+    mutate_at(vars(c(percent_sand, percent_silt, percent_clay)), round, 2) %>% 
     dplyr::select(sample_id, starts_with("percent"), notes) %>% 
     force()
   
 }
 
-
-texture = compute_soil_texture(dat = texture_data2)
+data_processed = 
+  data_raw %>% 
+  mutate_at(vars(-c(sample_id, date_started, skip, notes)), as.numeric) %>% 
+  filter(is.na(skip)) %>% 
+  compute_soil_texture()
