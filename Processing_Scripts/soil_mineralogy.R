b3c713b3a88536f114f9a1102ae799648d0daa41
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2025-02-28 13:32:39 -0500

    Add missing sample checks

--- Processing_Scripts/soil_mineralogy.R
+++ Processing_Scripts/soil_mineralogy.R
@@ -61,6 +61,29 @@ xrd_processed =
          campaign = "EC1") %>% 
   dplyr::select(campaign, kit_id, transect_location, everything()) %>% 
   arrange(kit_id, transect_location) %>% 
-  force()
+  force() %>% 
+  # switch wetland and transition names due to a...
+  # ...sampling error: wetland soil was sampled and put into a jar labeled "transition" incorrectly
+  mutate(transect_location = case_when(kit_id == "K046" & transect_location == "transition" ~ "wetland", 
+                                       kit_id == "K046" & transect_location == "wetland" ~ "transition", 
+                                       TRUE ~ transect_location)) 
 
-xrd_processed %>% write.csv("Data/Processed/EC1_soil_mineralogy_2025-02-19.csv", na = "", row.names = F)
+# Make all columns headers lowercase
+names(xrd_processed) <- tolower(names(xrd_processed))
+
+# 
+# Missing sample check ---------------------------------------------------------
+
+source("./Processing_Scripts/Metadata_kit_list.R")
+
+metadata_collected %>% 
+  filter(sample_method == "jar", sample_type == "soil") -> meta_filter
+
+xrd_processed %>% 
+  full_join(meta_filter, by = c("campaign", "kit_id", "transect_location")) %>% 
+  mutate(notes = case_when(collected == FALSE & is.na(albite) ~ "sample not collected",
+                           collected == TRUE & is.na(notes) & is.na(albite) ~ "sample not analyzed",
+                          .default = notes)) %>% 
+  select(-c("sample_type", "collected", "sample_method")) -> xrd_full
+
+xrd_full %>% write.csv("Data/Processed/ec1_soil_mineralogy_2025-02-28.csv", na = "", row.names = F)
