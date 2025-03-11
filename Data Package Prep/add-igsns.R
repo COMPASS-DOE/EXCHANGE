2ed5b9a3914251a886efaa9a6943115d22042adf
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2025-03-10 20:56:26 -0400

    Need to add comments before PR

--- Data Package Prep/add-igsns.R
+++ Data Package Prep/add-igsns.R
@@ -30,30 +30,53 @@ multiple <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1e
 
 igsn <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1eidz4hdJqOQYucTWFrDOwtQGXWfway9iZh-fmsElE1w/edit?usp=sharing", "igsn")
 
-download_directory <- "https://drive.google.com/drive/u/1/folders/1MvCS-u6S47XbVLvTGZ-V7RXTwF6Nb98k"
-upload_directory <- ""
+download_directory <- "https://drive.google.com/drive/u/1/folders/1M-ASGuRoKqswiKbUWylWzoAyUmMPm367"
+save_directory <- "./Data/IGSN/"
 
 ## Identify data files
-regex <- "^ec1_(soil|sediment|water).*\\.csv$"
+regex <- "^ec1_soil.*\\.csv$"
 
-files <- drive_ls(directory, recursive = TRUE) %>% 
+files <- drive_ls(download_directory, recursive = TRUE) %>% 
   filter(grepl(regex, name)) %>% 
   filter(!name %in% c("ec1_water_fticrms_L2.csv", "ec1_water_fticrms_meta_L2.csv"))
 
 
 # 2. Process -------------------------------------------------------------------
-add_igsn <- function(files) {
-  
-  drive_download() 
-  
-  read_csv(file) %>% 
-    mutate(file_name = name) %>% 
-    left_join(mapping, by = "file_name") %>% 
+add_igsn <- function(filename) {
+
+    read_csv(filename) %>% 
+    mutate(file_name = filename) %>% 
+    left_join(mapping, by = "file_name") -> t
+
+  # need to figure out how to index into mapping based on filename
+  if(mapping$multiple_ids[mapping$file_name == filename] == TRUE) {
+    multiple %>% 
+      filter(file_name == filename) -> multiple_file
+    
+    t %>% 
+      left_join(multiple_file, by = c("campaign", "kit_id", "transect_location", "file_name")) %>% 
+      mutate(mapping_id = case_when(!is.na(new_mapping_id) ~ new_mapping_id,
+                                    .default = mapping_id)) %>% 
+      select(-new_mapping_id) -> t
+    
+  }
+  t %>% 
     mutate(sample_name = toupper(paste(campaign, kit_id, transect_location, mapping_id, sep = "_"))) %>% 
     left_join(igsn, by = "sample_name") %>% 
     select(-c(file_name, multiple_ids, mapping_id, file_path, sample_name)) %>% 
-    relocate(igsn, .before = campaign)
+    relocate(igsn, .before = campaign) %>% 
+    write_csv(file = paste0(save_directory, file_name))
+}
+
+for(i in 1:nrow(files)) {
   
+  drive_download(files$id[i], overwrite = TRUE)
+  
+  file_name <- paste(files$name[i])
+  
+  add_igsn(file_name)
+  
+  file.remove(file_name)
 }
 
 
